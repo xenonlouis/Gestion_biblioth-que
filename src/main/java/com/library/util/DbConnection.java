b17f037c04eb62900fef849f2ec08bc370629f88
Xenonlouis <batatano1@gmail.com>
2024-12-20 21:51:35 +0100

    fixed jenkins file

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -34,8 +34,6 @@ public class DbConnection {
         TYPE_MAPPING.put(double.class, "DOUBLE");
         TYPE_MAPPING.put(Boolean.class, "BOOLEAN");
         TYPE_MAPPING.put(boolean.class, "BOOLEAN");
-
-
     }
 
     public static Connection getConnection() throws SQLException {
